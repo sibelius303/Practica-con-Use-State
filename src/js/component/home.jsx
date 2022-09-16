4f8ed4c06e94355e9240d5a9857af5bdb1ed6538
sibelius303 <cesarev17@gmail.com>
2022-09-16 02:15:15 +0000

    Practica de useState usando un boton y texto

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [texto, setTexto] = useState('Hello World');
+	const textoPantalla = ['Soy un texto', 'Practica y seras el mejor', 'Vas a conseguir trabajo rapido'];
+
+	const cambiarTexto  = ()=>{
+		if (texto === 'Hello World') {
+			setTexto ('Soy un texto');
+		} else if (texto === 'Soy un texto') {
+			setTexto ('Practica y seras el mejor');
+		} else if (texto === 'Practica y seras el mejor') {
+			setTexto ('Vas a conseguir trabajo rapido');
+
+		} else {
+			setTexto ('Hello World')
+		}
+	};
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+
+		<div className="container">
+			<div className="pantalla">{texto}</div>
+			<button onClick={()=>cambiarTexto()} className="button">Click para cambiar Texto</button>
 		</div>
 	);
 };
